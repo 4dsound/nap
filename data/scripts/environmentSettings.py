e75b901280f6ce6b6b799f78c0d03d0b0d2b3233
CasimirGeelhoed <casimirgeelhoed@gmail.com>
2021-02-23 15:59:03 +0100

    remove entity in scene

--- data/scripts/environmentSettings.py
+++ data/scripts/environmentSettings.py
@@ -3,10 +3,10 @@
 GROUPS_COUNT = 1
 #############################################################################
 # The number of sound objects that do not receive from other sound objects.
-SOURCES_COUNT = 0
+SOURCES_COUNT = 4
 #############################################################################
 # The number of sound objects that receive from all other sound objects.
-SPACES_COUNT = 1
+SPACES_COUNT = 4
 #############################################################################
 # Set the maximum particle count for one source sound object.
 SOURCES_MAX_PARTICLE_COUNT = 27
