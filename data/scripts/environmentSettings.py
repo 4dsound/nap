a22944af0cb65d0b0b1a3197922ec247c49308c9
Stijn van Beek <stijn.v.beek@gmail.com>
2021-05-26 11:45:47 +0200

    Update environmentSettings.py

--- data/scripts/environmentSettings.py
+++ data/scripts/environmentSettings.py
@@ -1,16 +1,14 @@
-#############################################################################
 # The number of group transformations.
 GROUPS_COUNT = 1
-#############################################################################
+
 # The number of sound objects that do not receive from other sound objects.
 SOURCES_COUNT = 4
-#############################################################################
+
 # The number of sound objects that receive from all other sound objects.
 SPACES_COUNT = 4
-#############################################################################
+
 # Set the maximum particle count for one source sound object.
-SOURCES_MAX_PARTICLE_COUNT = 27
-#############################################################################
+SOURCES_MAX_PARTICLE_COUNT = 128
+
 # Set the maximum particle count for one space sound object.
-SPACES_MAX_PARTICLE_COUNT = 27
-#############################################################################
+SPACES_MAX_PARTICLE_COUNT = 128
