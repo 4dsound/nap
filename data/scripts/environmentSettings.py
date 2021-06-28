286af114843f69ff7537d4cbeddc82423e83d69b
Stijn van Beek <stijn.v.beek@gmail.com>
2021-06-28 15:14:34 +0200

    Update environmentSettings.py

--- data/scripts/environmentSettings.py
+++ data/scripts/environmentSettings.py
@@ -2,19 +2,19 @@
 GROUPS_COUNT = 1
 
 # The number of sound objects that do not receive from other sound objects.
-SOURCES_COUNT = 1
+SOURCES_COUNT = 4
 
 # The number of sound objects that receive from all other sound objects.
-SPACES_COUNT = 1
+SPACES_COUNT = 4
 
 # Number of sound objects with spatial delay and reverb47 mapped to speaker
 REVERB_COUNT = 1
 
 # Set the maximum particle count for one source sound object.
-SOURCES_MAX_PARTICLE_COUNT = 27
+SOURCES_MAX_PARTICLE_COUNT = 128
 
 # Set the maximum particle count for one space sound object.
-SPACES_MAX_PARTICLE_COUNT = 27
+SPACES_MAX_PARTICLE_COUNT = 128
 
 # Maximum number of speakers for reverb sound objects
 REVERB_MAX_PARTICLE_COUNT = 64
