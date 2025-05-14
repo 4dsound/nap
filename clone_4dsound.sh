3f90e173980f8a5b3bbeb600224d7f7af093723a
Stijn van Beek <stijn.v.beek@gmail.com>
2025-05-14 12:24:57 +0200

    Create clone_4dsound.sh

---
+++ clone_4dsound.sh
@@ -0,0 +1,5 @@
+git clone https://www.github.com/4dsound/4dsound_modules modules
+git clone https://www.github.com/4dsound/napaudioadvanced modules/napaudioadvanced
+git clone https://www.github.com/4dsound/napvban modules/napvban
+git clone https://www.github.com/4dsound/vban modules/napvban/thirdparty/vban
+git clone https://www.github.com/4dsound/4dsound apps/4dsound
