2e9a726ce9feb0a3cae8afc2edddee3781823cba
Stijn van Beek <stijn.v.beek@gmail.com>
2025-08-17 23:06:34 +0200

    Add notarization

--- package_4dsound.sh
+++ package_4dsound.sh
@@ -2,7 +2,7 @@
 rm apps/4dsound/data/settings.json
 
 if [ "$(uname)" = "Darwin" ]; then
-  sh package_app.sh 4dsound build "4DSOUND Technologies BV"
+  sh package_app.sh 4dsound build "4DSOUND Technologies BV" "NotaryProfile"
 else
   sh package_app.sh 4dsound build
 fi
