b566c0001a9e7f958e2383f394f68be596913c40
Stijn van Beek <stijn.v.beek@gmail.com>
2025-09-05 17:08:12 +0200

    Package suite

--- package_4dsound.sh
+++ package_4dsound.sh
@@ -1,5 +1,6 @@
 # Edit these values locally
 qt_dir="~/Qt/6.6.1/macos" # QT location
+m4l_directory="/Users/stijn/Documents/4DSOUND/4DS m4l"
 code_signature="4DSOUND Technologies BV" # MacOS code signature
 notary_profile="NotaryProfile" # Notarization credentials
 
@@ -13,16 +14,16 @@ fi
 # Delete settings file
 rm apps/4dsound/data/settings.json
 
+# Grab the app title and version number
+title=`jq -r '.Title' apps/4dsound/app.json`
+version=`jq -r '.Version' apps/4dsound/app.json`
+
 if [ "$(uname)" = "Darwin" ]; then
   # MacOS
 
   # Package and codesign
   export QT_DIR=${qt_dir}
   sh package_app.sh 4dsound build "${code_signature}" "${notary_profile}"
-
-  # Grab the app title and version number
-  title=`jq -r '.Title' apps/4dsound/app.json`
-  version=`jq -r '.Version' apps/4dsound/app.json`
 
   cd install
 
@@ -53,10 +54,40 @@ if [ "$(uname)" = "Darwin" ]; then
   --notarize "${notary_profile}" \
   "${dmg_filename}" \
   "${app_bundle}"
+
+  suite_dir="4DSOUND Suite MacOS"
+  suite_zip="4DSOUND Suite MacOS.zip"
+  mkdir "${suite_dir}"
+  cp -r "${m4l_directory}" "${suite_dir}"
+  cp "${dmg_filename}" "${suite_dir}/${dmg_filename}"
+  zip -r -q "${suite_zip}" "${suite_dir}"
+  rm -rf "${suite_dir}"
+
   cd ..
 
-else
-  # Windows and Linux
-  set QT_DIR=${QT_DIR}
+elif [ "$(uname)" = "Linux" ]; then
+  # Linux
+  qt_dir="~/Qt/6.6.1/macos" # QT location
+  set QT_DIR=${qt_dir}
   sh package_app.sh 4dsound build
+
+  app_zip="${title} ${version} Linux.zip"
+
+else
+  # Windows
+  set QT_DIR=${qt_dir}
+  sh package_app.sh 4dsound build
+
+  cd install
+
+  app_zip="${title} ${version} Win.zip"
+  suite_dir="4DSOUND Suite Win"
+  suite_zip="4DSOUND Suite Win.zip"
+  mkdir "${suite_dir}"
+  cp -r "${m4l_directory}" "${suite_dir}"
+  cp "${app_zip}" "${suite_dir}/${app_zip}"
+  zip -r -q "${suite_zip}" "${suite_dir}"
+  rm -rf "${suite_dir}"
+
+  cd ..
 fi
