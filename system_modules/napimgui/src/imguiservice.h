eeb2e2e18c2a65d335882ea831a2db00b37a2ea5
Casimir Geelhoed <casimirgeelhoed@gmail.com>
2026-09-21 14:35:39 +0200

    re-add copy paste support

--- system_modules/napimgui/src/imguiservice.h
+++ system_modules/napimgui/src/imguiservice.h
@@ -326,8 +326,8 @@ namespace nap
 
 			std::array<bool,3> mMousePressed				= { false };					///< If the mouse was pressed this frame
 			std::array<bool,3> mMouseRelease				= { false };					///< If the mouse was released this frame
-			std::array<bool,3> mModPressed					= { false };					///< If the ctrl (0), alt (1) or shift (2) modifier key is pressed
-			std::array<bool,3> mModRelease					= { false };					///< If the ctrl (0), alt (1) or shift (2) modifier key is released
+			std::array<bool,4> mModPressed					= { false };					///< If the ctrl (0), alt (1) or shift (2) modifier key is pressed
+			std::array<bool,4> mModRelease					= { false };					///< If the ctrl (0), alt (1) or shift (2) modifier key is released
 			std::array<int, 3> mPointerID					= { gui::pointerInvalidID };	///< Pointer id, -1 for pointer, 0+ for finger
 			KeyArray mKeyPressed							= { false };					///< The keys that were pressed this frame
 			std::vector<int> mKeyRelease;													///< The keys that were released this frame
